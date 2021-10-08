0310a9721cf02f197d3b32c0a3c56dfa79974ed3
bgoonz <bryan.guner@gmail.com>
2021-10-08 12:30:00 +0000

    -update

---
+++ EDUCATION/OOP-JavaScript-master/OOP-JavaScript-master/02-objects/03-constructors.js
@@ -0,0 +1,11 @@
+
+// Constructor function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = () => {
+    console.log('draw');
+  }
+}
+
+const circle = new Circle(1);
+circle.draw();
