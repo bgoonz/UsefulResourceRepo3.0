24f987ceecb87b12252fd8edef596fe0f11746a1
bgoonz <bryan.guner@gmail.com>
2021-10-08 12:48:32 +0000

    -update

--- TOOLS/STOCK-REUSABLE-TOOLS/bootstrap-3.4.1/bootstrap-3.4.1/docs/assets/js/src/search.js
+++ TOOLS/STOCK-REUSABLE-TOOLS/bootstrap-3.4.1/bootstrap-3.4.1/docs/assets/js/src/search.js
@@ -3,49 +3,49 @@
 // ++++++++++++++++++++++++++++++++++++++++++
 
 (function () {
-  'use strict'
+  "use strict";
 
-  var inputElement = document.getElementById('search-input')
+  var inputElement = document.getElementById("search-input");
 
   if (!window.docsearch || !inputElement) {
-    return
+    return;
   }
 
   function getOrigin() {
-    var location = window.location
-    var origin = location.origin
+    var location = window.location;
+    var origin = location.origin;
 
     if (!origin) {
-      var port = location.port ? ':' + location.port : ''
+      var port = location.port ? ":" + location.port : "";
 
-      origin = location.protocol + '//' + location.hostname + port
+      origin = location.protocol + "//" + location.hostname + port;
     }
 
-    return origin
+    return origin;
   }
 
   window.docsearch({
-    apiKey: 'c8948afa20e6437a6e829f7e87b9ac11',
-    indexName: 'bootstrap-v3',
-    inputSelector: '#search-input',
+    apiKey: "c8948afa20e6437a6e829f7e87b9ac11",
+    indexName: "bootstrap-v3",
+    inputSelector: "#search-input",
     transformData: function (hits) {
       return hits.map(function (hit) {
-        var siteurl = getOrigin()
-        var urlRE = /^https?:\/\/getbootstrap\.com/
+        var siteurl = getOrigin();
+        var urlRE = /^https?:\/\/getbootstrap\.com/;
 
         // When in production, return the result as is,
         // otherwise remove our url from it.
-        hit.url = siteurl.match(urlRE) ? hit.url : hit.url.replace(urlRE, '')
+        hit.url = siteurl.match(urlRE) ? hit.url : hit.url.replace(urlRE, "");
 
         // Prevent jumping to first header
-        if (hit.anchor === 'content') {
-          hit.url = hit.url.replace(/#content$/, '')
-          hit.anchor = null
+        if (hit.anchor === "content") {
+          hit.url = hit.url.replace(/#content$/, "");
+          hit.anchor = null;
         }
 
-        return hit
-      })
+        return hit;
+      });
     },
-    debug: false // Set debug to true if you want to inspect the dropdown
-  })
-}())
+    debug: false, // Set debug to true if you want to inspect the dropdown
+  });
+})();
--- TOOLS/STOCK-REUSABLE-TOOLS/bootstrap-3.4.1/bootstrap-3.4.1/js/tests/unit/alert.js
+++ TOOLS/STOCK-REUSABLE-TOOLS/bootstrap-3.4.1/bootstrap-3.4.1/js/tests/unit/alert.js
@@ -1,83 +1,106 @@
 $(function () {
-  'use strict';
+  "use strict";
 
-  QUnit.module('alert plugin')
+  QUnit.module("alert plugin");
 
-  QUnit.test('should be defined on jquery object', function (assert) {
-    assert.expect(1)
-    assert.ok($(document.body).alert, 'alert method is defined')
-  })
+  QUnit.test("should be defined on jquery object", function (assert) {
+    assert.expect(1);
+    assert.ok($(document.body).alert, "alert method is defined");
+  });
 
-  QUnit.module('alert', {
+  QUnit.module("alert", {
     beforeEach: function () {
       // Run all tests in noConflict mode -- it's the only way to ensure that the plugin works in noConflict mode
-      $.fn.bootstrapAlert = $.fn.alert.noConflict()
+      $.fn.bootstrapAlert = $.fn.alert.noConflict();
     },
     afterEach: function () {
-      $.fn.alert = $.fn.bootstrapAlert
-      delete $.fn.bootstrapAlert
+      $.fn.alert = $.fn.bootstrapAlert;
+      delete $.fn.bootstrapAlert;
+    },
+  });
+
+  QUnit.test("should provide no conflict", function (assert) {
+    assert.expect(1);
+    assert.strictEqual(
+      $.fn.alert,
+      undefined,
+      "alert was set back to undefined (org value)"
+    );
+  });
+
+  QUnit.test(
+    "should return jquery collection containing the element",
+    function (assert) {
+      assert.expect(2);
+      var $el = $("<div/>");
+      var $alert = $el.bootstrapAlert();
+      assert.ok($alert instanceof $, "returns jquery collection");
+      assert.strictEqual($alert[0], $el[0], "collection contains element");
     }
-  })
+  );
 
-  QUnit.test('should provide no conflict', function (assert) {
-    assert.expect(1)
-    assert.strictEqual($.fn.alert, undefined, 'alert was set back to undefined (org value)')
-  })
+  QUnit.test("should fade element out on clicking .close", function (assert) {
+    assert.expect(1);
+    var alertHTML =
+      '<div class="alert alert-danger fade in">' +
+      '<a class="close" href="#" data-dismiss="alert">×</a>' +
+      "<p><strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.</p>" +
+      "</div>";
+    var $alert = $(alertHTML).bootstrapAlert();
 
-  QUnit.test('should return jquery collection containing the element', function (assert) {
-    assert.expect(2)
-    var $el = $('<div/>')
-    var $alert = $el.bootstrapAlert()
-    assert.ok($alert instanceof $, 'returns jquery collection')
-    assert.strictEqual($alert[0], $el[0], 'collection contains element')
-  })
+    $alert.find(".close").trigger("click");
 
-  QUnit.test('should fade element out on clicking .close', function (assert) {
-    assert.expect(1)
-    var alertHTML = '<div class="alert alert-danger fade in">'
-        + '<a class="close" href="#" data-dismiss="alert">×</a>'
-        + '<p><strong>Holy guacamole!</strong> Best check yo self, you\'re not looking too good.</p>'
-        + '</div>'
-    var $alert = $(alertHTML).bootstrapAlert()
+    assert.strictEqual(
+      $alert.hasClass("in"),
+      false,
+      "remove .in class on .close click"
+    );
+  });
 
-    $alert.find('.close').trigger('click')
+  QUnit.test("should remove element when clicking .close", function (assert) {
+    assert.expect(2);
+    var done = assert.async();
 
-    assert.strictEqual($alert.hasClass('in'), false, 'remove .in class on .close click')
-  })
+    var alertHTML =
+      '<div class="alert alert-danger fade in">' +
+      '<a class="close" href="#" data-dismiss="alert">×</a>' +
+      "<p><strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.</p>" +
+      "</div>";
+    var $alert = $(alertHTML).appendTo("#qunit-fixture").bootstrapAlert();
 
-  QUnit.test('should remove element when clicking .close', function (assert) {
-    assert.expect(2)
-    var done = assert.async()
+    assert.notEqual(
+      $("#qunit-fixture").find(".alert").length,
+      0,
+      "element added to dom"
+    );
 
-    var alertHTML = '<div class="alert alert-danger fade in">'
-        + '<a class="close" href="#" data-dismiss="alert">×</a>'
-        + '<p><strong>Holy guacamole!</strong> Best check yo self, you\'re not looking too good.</p>'
-        + '</div>'
-    var $alert = $(alertHTML).appendTo('#qunit-fixture').bootstrapAlert()
+    $alert.on("closed.bs.alert", function () {
+      assert.strictEqual(
+        $("#qunit-fixture").find(".alert").length,
+        0,
+        "element removed from dom"
+      );
+      done();
+    });
 
-    assert.notEqual($('#qunit-fixture').find('.alert').length, 0, 'element added to dom')
+    $alert.find(".close").trigger("click");
+  });
 
-    $alert.on('closed.bs.alert', function () {
-      assert.strictEqual($('#qunit-fixture').find('.alert').length, 0, 'element removed from dom')
-      done()
-    })
-
-    $alert.find('.close').trigger('click')
-  })
-
-  QUnit.test('should not fire closed when close is prevented', function (assert) {
-    assert.expect(1)
-    var done = assert.async()
-    $('<div class="alert"/>')
-      .on('close.bs.alert', function (e) {
-        e.preventDefault()
-        assert.ok(true, 'close event fired')
-        done()
-      })
-      .on('closed.bs.alert', function () {
-        assert.ok(false, 'closed event fired')
-      })
-      .bootstrapAlert('close')
-  })
-
-})
+  QUnit.test(
+    "should not fire closed when close is prevented",
+    function (assert) {
+      assert.expect(1);
+      var done = assert.async();
+      $('<div class="alert"/>')
+        .on("close.bs.alert", function (e) {
+          e.preventDefault();
+          assert.ok(true, "close event fired");
+          done();
+        })
+        .on("closed.bs.alert", function () {
+          assert.ok(false, "closed event fired");
+        })
+        .bootstrapAlert("close");
+    }
+  );
+});
