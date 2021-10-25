dfc539e00b3e263607ecc6439ed1ddf150c9e556
Bryan C Guner <bryan.guner@gmail.com>
2021-10-25 11:01:50 +0000

    -update

---
+++ EDUCATION/DS_ALGO/javascript-datastructures-algorithms-main/test/js/algorithms/greedy/longest-common-subsequence.spec.js
@@ -0,0 +1,12 @@
+import "mocha";
+import { expect } from "chai";
+import { lcsRecursive } from "../../../../src/js/index";
+
+describe("LCS Greedy", () => {
+  it("works with Greedy approach", () => {
+    const wordX = "acbaed";
+    const wordY = "abcadf";
+
+    expect(lcsRecursive(wordX, wordY)).to.equal(4);
+  });
+});
